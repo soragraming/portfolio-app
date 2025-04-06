72f1a46ead08f1480991e1e5deab8a61c04daa0a
池田大空 <ohzora@Mac-mini.local>
2025-04-06 21:21:58 +0900

    Enable db.create_all() on startup

--- app.py
+++ app.py
@@ -251,6 +251,8 @@ def load_user(user_id):
 # ✅ 最後に Flask アプリ起動。おまじないのようなもの
 # 先にアプリの設定（最上部）やルート定義（ルーティング）が必要
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=5001)
 
 # よくある構成
