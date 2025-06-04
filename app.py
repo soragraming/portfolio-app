0582e24290e37ce2e9eae43012cc1bce957d4a05
Ohzora Ikeda <ohzora0817@gmail.com>
2025-06-04 23:29:28 +0900

    Render用設定やDB修正を反映

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     is_confirmed = db.Column(db.Boolean, default=False)
     posts = db.relationship('Post', backref='author', lazy=True,cascade="all, delete-orphan")
 
